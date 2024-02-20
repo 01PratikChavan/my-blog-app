ab97f72990c7fcd63e475fcac4dc1283667e593f
01PratikChavan <pratikchavan470@gmail.com>
2024-02-20 21:43:27 +0530

    done with comment section

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+export default function Dashboard() {
+  
+  const location= useLocation();
+  const [tab,setTab]=useState('');
+ 
+  useEffect(()=>{
+    const urlFromSearch = new URLSearchParams(location.search);
+    const tablFromUrl = urlFromSearch.get('tab');
+    if(tablFromUrl){
+      setTab(tablFromUrl);
+    }
+  },[location.search]);
+  
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row" >
+     <div className="md:w-60">
+        <DashSidebar />
+     </div>
+     { tab==='profile' && <DashProfile /> }
+     {tab==='posts' && <DashPosts />}
+     {tab==='users' && <DashUsers />}
+    </div>
+  )
+}
